34864069a5b23efa790bfbf5d401b0c3f4c0b14e
sara-p- <sarapitt88@gmail.com>
2026-04-21 17:36:22 -0400

    Features - Custom Styles and typography changes

---
+++ patterns/cards-1.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Cards 1
+ * Slug: /cards-1
+ * Categories: Cards
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Section"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Green Container"},"align":"wide","style":{"border":{"radius":{"topLeft":"40px","topRight":"40px","bottomLeft":"40px","bottomRight":"40px"}},"spacing":{"padding":{"top":"var:preset|spacing|32","bottom":"var:preset|spacing|32","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"light-green","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide has-light-green-background-color has-background" style="border-top-left-radius:40px;border-top-right-radius:40px;border-bottom-left-radius:40px;border-bottom-right-radius:40px;padding-top:var(--wp--preset--spacing--32);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--32);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"metadata":{"name":"Headings"},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":4,"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-green"}}}},"textColor":"dark-green"} -->
+<h4 class="wp-block-heading has-text-align-center has-dark-green-color has-text-color has-link-color">Choosing Willow</h4>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-green"}}}},"textColor":"dark-green"} -->
+<h1 class="wp-block-heading has-text-align-center has-dark-green-color has-text-color has-link-color">Why Families Choose Willow</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Cards Container"},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Single Cards Style 1","patternName":"core/block/151"},"style":{"border":{"radius":{"topLeft":"30px","topRight":"30px","bottomLeft":"30px","bottomRight":"30px"}},"spacing":{"padding":{"top":"var:preset|spacing|16","bottom":"var:preset|spacing|16","left":"var:preset|spacing|16","right":"var:preset|spacing|16"},"blockGap":"0"}},"backgroundColor":"warm-white","layout":{"type":"default"}} -->
+<div class="wp-block-group has-warm-white-background-color has-background" style="border-top-left-radius:30px;border-top-right-radius:30px;border-bottom-left-radius:30px;border-bottom-right-radius:30px;padding-top:var(--wp--preset--spacing--16);padding-right:var(--wp--preset--spacing--16);padding-bottom:var(--wp--preset--spacing--16);padding-left:var(--wp--preset--spacing--16)"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/icon-ipad.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-green"}}}},"textColor":"dark-green"} -->
+<h5 class="wp-block-heading has-dark-green-color has-text-color has-link-color">Simple, Easy Online Arrangements</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-green"}}}},"textColor":"dark-green"} -->
+<p class="has-dark-green-color has-text-color has-link-color">No appointments. No waiting.  Start and finish on your schedule</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/hero-main.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Title: Hero Main
+ * Slug: /hero-main
+ * Categories: heros
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Hero"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|160","bottom":"var:preset|spacing|160"}},"background":{"backgroundImage":{"url":"http://ollie-wp-course.local/wp-content/uploads/2026/04/willow-tree-bg.jpg","id":106,"source":"file","title":"willow-tree-bg"},"backgroundSize":"cover"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="min-height:100%;padding-top:var(--wp--preset--spacing--160);padding-bottom:var(--wp--preset--spacing--160)"><!-- wp:group {"metadata":{"name":"Wrapper"},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"metadata":{"name":"Content"},"style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Subheading"},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<h4 class="wp-block-heading" style="margin-top:0">Available 24/7 for immediate assistance.</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"bottom":"0"}}}} -->
+<h1 class="wp-block-heading" style="margin-bottom:0">Simple, Dignified Cremation Services</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Text"},"style":{"spacing":{"blockGap":"0","margin":{"bottom":"var:preset|spacing|32"}}},"layout":{"type":"constrained","contentSize":"420px","justifyContent":"left"}} -->
+<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--32)"><!-- wp:paragraph -->
+<p>We provide affordable, direct cremation arrangements from the comfort of your home. No funeral home visits required.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|16"},"margin":{"top":"0","bottom":"var:preset|spacing|32"}}}} -->
+<div class="wp-block-buttons" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--32)"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get instant quote</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">How it works</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:group {"metadata":{"name":"Callout"},"style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Section 1"},"style":{"spacing":{"blockGap":"0","padding":{"right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-right:0"><!-- wp:image {"sizeSlug":"large","linkDestination":"none","style":{"spacing":{"margin":{"right":"5px","bottom":"5px"}}}} -->
+<figure class="wp-block-image size-large" style="margin-right:5px;margin-bottom:5px"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/check-mark.svg" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<h6 class="wp-block-heading">Transparent pricing</h6>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Section 2"},"style":{"spacing":{"blockGap":"0","padding":{"right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-right:0"><!-- wp:image {"sizeSlug":"large","linkDestination":"none","style":{"spacing":{"margin":{"right":"5px","bottom":"5px"}}}} -->
+<figure class="wp-block-image size-large" style="margin-right:5px;margin-bottom:5px"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/check-mark.svg" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<h6 class="wp-block-heading">World-class care team</h6>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Section 3"},"style":{"spacing":{"blockGap":"0","padding":{"right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-right:0"><!-- wp:image {"sizeSlug":"large","linkDestination":"none","style":{"spacing":{"margin":{"right":"5px","bottom":"5px"}}}} -->
+<figure class="wp-block-image size-large" style="margin-right:5px;margin-bottom:5px"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/check-mark.svg" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<h6 class="wp-block-heading">100% Online Arrangement</h6>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
