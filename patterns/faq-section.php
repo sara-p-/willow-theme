1d9648cade8a8378447191956cee84ce455ed4f3
sara-p- <sarapitt88@gmail.com>
2026-04-23 19:18:18 -0400

    Features - New Patterns

---
+++ patterns/faq-section.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: FAQ Section
+ * Slug: /faq-section
+ * Categories: FAQ
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Process"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|120","bottom":"var:preset|spacing|160"}}},"gradient":"green-to-white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-green-to-white-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--120);padding-bottom:var(--wp--preset--spacing--160)"><!-- wp:group {"metadata":{"name":"Wrapper"},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"500px"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"margin":{"right":"var:preset|spacing|32","left":"0"}}}} -->
+<figure class="wp-block-image size-full" style="margin-right:var(--wp--preset--spacing--32);margin-left:0"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/faq-image-resized.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"metadata":{"name":"Content"},"style":{"layout":{"selfStretch":"fixed","flexSize":"650px"},"spacing":{"blockGap":"var:preset|spacing|60","padding":{"top":"var:preset|spacing|32"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--32)"><!-- wp:group {"metadata":{"name":"Text"},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<h1 class="wp-block-heading has-black-color has-text-color has-link-color">Willow Pricing: Frequently Asked Questions</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|red"}}},"spacing":{"margin":{"bottom":"var:preset|spacing|48"}}},"textColor":"red"} -->
+<h2 class="wp-block-heading has-red-color has-text-color has-link-color" style="margin-bottom:var(--wp--preset--spacing--48)">Costs, Options &amp; What to Expect</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<p class="has-black-color has-text-color has-link-color">Get clear answers about cremation costs, what’s included, and how pricing works. We make it easy to understand your options so you can make decisions with confidence.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:accordion -->
+<div role="group" class="wp-block-accordion"><!-- wp:accordion-item {"className":"faq-accordion-item","style":{"elements":{"link":{"color":{"text":"var:preset|color|red"}}},"border":{"width":"1px","radius":{"topLeft":"30px","topRight":"30px","bottomLeft":"30px","bottomRight":"30px"}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"red"} -->
+<div class="wp-block-accordion-item faq-accordion-item has-red-color has-text-color has-link-color" style="border-width:1px;border-top-left-radius:30px;border-top-right-radius:30px;border-bottom-left-radius:30px;border-bottom-right-radius:30px;margin-top:0;margin-bottom:0"><!-- wp:accordion-heading {"className":"faq-accordion-header","style":{"typography":{"fontSize":"25px"},"border":{"width":"0px","style":"none","radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}},"spacing":{"padding":{"right":"var:preset|spacing|16","left":"var:preset|spacing|16","top":"var:preset|spacing|16","bottom":"var:preset|spacing|16"}}},"fontFamily":"ar-one-sans"} -->
+<h3 class="wp-block-accordion-heading faq-accordion-header has-ar-one-sans-font-family" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;font-size:25px"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--16);padding-right:var(--wp--preset--spacing--16);padding-bottom:var(--wp--preset--spacing--16);padding-left:var(--wp--preset--spacing--16)"><span class="wp-block-accordion-heading__toggle-title">How much does cremation cost?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
+<!-- /wp:accordion-heading -->
+
+<!-- wp:accordion-panel {"className":"faq-accordion-panel","style":{"spacing":{"padding":{"right":"var:preset|spacing|16","left":"var:preset|spacing|16","bottom":"var:preset|spacing|16"}},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<div role="region" class="wp-block-accordion-panel faq-accordion-panel has-black-color has-text-color has-link-color" style="padding-right:var(--wp--preset--spacing--16);padding-bottom:var(--wp--preset--spacing--16);padding-left:var(--wp--preset--spacing--16)"><!-- wp:paragraph -->
+<p>The cost of cremation can vary based on location and services, but direct cremation is typically the most affordable option. At Willow, we offer clear, upfront pricing so you know exactly what to expect—no hidden fees.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:accordion-panel --></div>
+<!-- /wp:accordion-item --></div>
+<!-- /wp:accordion --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/single-testimonial.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: Single Testimonial
+ * Slug: /single-testimonial
+ * Categories: Testimonials
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Single Testimonial"},"className":"is-style-green-on-white","style":{"border":{"radius":{"topLeft":"40px","topRight":"40px","bottomLeft":"40px","bottomRight":"40px"}},"spacing":{"padding":{"top":"var:preset|spacing|32","bottom":"var:preset|spacing|32","left":"var:preset|spacing|32","right":"var:preset|spacing|32"},"blockGap":"var:preset|spacing|32"}},"backgroundColor":"white","layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-green-on-white has-white-background-color has-background" style="border-top-left-radius:40px;border-top-right-radius:40px;border-bottom-left-radius:40px;border-bottom-right-radius:40px;padding-top:var(--wp--preset--spacing--32);padding-right:var(--wp--preset--spacing--32);padding-bottom:var(--wp--preset--spacing--32);padding-left:var(--wp--preset--spacing--32)"><!-- wp:group {"metadata":{"name":"Stars"},"style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/icon-single-star-1.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/icon-single-star-1.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/icon-single-star-1.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/icon-single-star-1.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/icon-single-star-1.png" alt="" class=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"metadata":{"name":"Text"}} -->
+<p>"Heritage, John and the team were incredibly kind and thorough during a very difficult time for our family. They made everything simple for us and we can't thank (and recommend) them enough!"</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"metadata":{"name":"Citation"},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"metadata":{"name":"Name"}} -->
+<h5 class="wp-block-heading">Gretchen Farrell</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"metadata":{"name":"Location"},"className":"testimonials-location-text"} -->
+<p class="testimonials-location-text">Louisville</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
