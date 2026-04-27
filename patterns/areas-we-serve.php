d718a4cb4f778ab8747962a30d321ef6a54d1d9f
sara-p- <sarapitt88@gmail.com>
2026-04-27 18:23:25 -0400

    New patterns and styling

---
+++ patterns/areas-we-serve.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: Areas We Serve
+ * Slug: /areas-we-serve
+ * Categories: Banners
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Areas We Serve"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|48","bottom":"var:preset|spacing|48"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--48);padding-bottom:var(--wp--preset--spacing--48)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|32"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Areas We Serve</h3>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|16","left":"10px"}}}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Cleveland, OH</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Canton, OH</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Independence, OH</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Westlake, OH</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Akron, OH</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Beechwood, OH</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline-white"} -->
+<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link wp-element-button">Mahoning Valley, OH</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/side-by-side.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: Side by Side
+ * Slug: /side-by-side
+ * Categories: 2-Column Layout
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Side by Side"},"className":"is-style-red-on-light-pink","style":{"spacing":{"padding":{"top":"var:preset|spacing|120","bottom":"var:preset|spacing|120"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-red-on-light-pink" style="padding-top:var(--wp--preset--spacing--120);padding-bottom:var(--wp--preset--spacing--120)"><!-- wp:group {"metadata":{"name":"Wrapper"},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|32"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Text Content"},"style":{"spacing":{"blockGap":"var:preset|spacing|16"},"layout":{"selfStretch":"fixed","flexSize":"590px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Headings"},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":1,"metadata":{"name":"Subheading"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<h1 class="wp-block-heading has-black-color has-text-color has-link-color">Willow - Greater Cleveland</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Serving Northeast Ohio</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Willow offers direct cremation services across Northeast Ohio, including Greater Cleveland, Canton, Akron, Independence, Beechwood, Westlake, and  Mahoning Valley making it easy to arrange from anywhere.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"metadata":{"name":"Address Group"},"style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/location-pin-1-1-Vectorized.svg" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"metadata":{"name":"Address"},"className":"font-size-22","style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.35"}},"fontSize":"medium","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group font-size-22 has-medium-font-size" style="font-style:normal;font-weight:700;line-height:1.35"><!-- wp:paragraph -->
+<p>Greater Cleveland Office</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>16150 Brookpark Rd.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Cleveland, OH 44135 </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Phone Group"},"style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/telephone-1-1-Vectorized.svg" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"metadata":{"name":"Phone Number"},"className":"font-size-22","layout":{"type":"default"}} -->
+<div class="wp-block-group font-size-22"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong><a href="tel:2168650007">(216) 865-0007</a></strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fixed","flexSize":"500px"}}} -->
+<figure class="wp-block-image size-full"><img src="http://ollie-wp-course.local/wp-content/uploads/2026/04/side-by-side-ohio.png" alt="" class=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
